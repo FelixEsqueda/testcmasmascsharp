687841f1c20c18e49ff95f1f31153e541906bfd6
gbianchi <gbianchi@afip.gob.ar>
2019-02-08 15:46:25 -0300

    version que funciona

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,16 +10,22 @@ namespace ConsoleApp1
     class Program
     {
         [DllImport(@"C:\Users\u40497\Source\Repos\Mixed_Mode_Debugging\Debug\Mixed_Mode_Debugging.dll", EntryPoint =
-        "mixed_mode_multiply", CallingConvention = CallingConvention.StdCall)]
+        "mixed_mode_multiply", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
         public static extern int Multiply(StringBuilder buff, int length);
 
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder(20000); // 20000 caracteres.
+            StringBuilder sb = new StringBuilder(100); // 20000 caracteres.
 
             int result = Multiply(sb, sb.Capacity);
+            //sb.Length = 150;
 
-            Console.WriteLine(sb.ToString());
+            var lineas = sb.ToString().Split(new string[] { "&&" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var s in lineas)
+            {
+                Console.WriteLine(s);
+            }
+            //Console.WriteLine(sb.ToString());
 
             Console.ReadKey();
         }
--- Mixed_Mode_Debugging/Mixed_Mode.cpp
+++ Mixed_Mode_Debugging/Mixed_Mode.cpp
@@ -4,20 +4,29 @@
 extern "C"
 {
 	__declspec(dllexport) int __stdcall mixed_mode_multiply(wchar_t *buff, int length) {
-		char *destino = reinterpret_cast<char *>(buff);
+		//char *destino = reinterpret_cast<char *>(buff);
+		wchar_t *destino = buff;
 		*destino = 0;
 		const wchar_t *cadena1 = L"1esta es una cadena1";
 		const  wchar_t* cadena2 = L"2esta es una cadena2";
 		const  wchar_t* cadena3 = L"3esta es una cadena3";
 		const  wchar_t* cadena4 = L"4esta es una cadena4";
-		strncpy(destino, (const char *)cadena1, wcslen(cadena1) * sizeof(wchar_t));
-//		destino += ((wcslen(cadena1) + 1) * sizeof(wchar_t));
-		strncpy(destino, (const char *)cadena2, wcslen(cadena2) * sizeof(wchar_t));
-		/*destino += ((wcslen(cadena2) + 1) * sizeof(wchar_t));
-		strncpy(destino, (const char *)cadena3, wcslen(cadena3) * sizeof(wchar_t));
-		destino += ((wcslen(cadena3) + 1) * sizeof(wchar_t));
-		strncpy(destino, (const char *)cadena4, wcslen(cadena4) * sizeof(wchar_t));
-		destino += ((wcslen(cadena4) + 1) * sizeof(wchar_t));*/
+		const wchar_t* separador = L"&&";
+		wcscpy(destino,cadena1);
+		destino += (wcslen(cadena1) );
+		//Agrego separador
+		wcscpy(destino, separador);
+		destino += (wcslen(separador));
+		wcscpy(destino, cadena2);
+	    destino += (wcslen(cadena2) );
+		wcscpy(destino, separador);
+		destino += (wcslen(separador));
+		wcscpy(destino, cadena3);
+		destino += (wcslen(cadena3));
+		wcscpy(destino, separador);
+		destino += (wcslen(separador));
+		wcscpy(destino, cadena4);
+		destino += (wcslen(cadena4));
 		return 0;
 	}
 }
